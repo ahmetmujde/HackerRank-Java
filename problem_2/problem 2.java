788cd0f30b168dd041bdfe5caa75b896959c22ec
ahmetmujde <mujde.ahmet79@gmail.com>
2021-07-04 23:36:13 +0300

    problem2

---
+++ problem_2/problem 2.java
@@ -0,0 +1,21 @@
+//Task
+//In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line.
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+    }
+}
+
+
