ceda4a26de9c36057daa006246dd38f76a7bf298
ahmetmujde <mujde.ahmet79@gmail.com>
2021-07-09 16:50:43 +0300

    JavaStaticInitializerBlock

---
+++ JavaStaticInitializerBlock/JavaStaticInitializerBlock.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+static byte B;
+static byte H;
+static boolean flag=true;
+
+static {
+    Scanner scanner = new Scanner(System.in);
+    
+    B=scanner.nextByte();
+    H=scanner.nextByte();
+    
+    scanner.close();
+    
+    try{
+        if(B<=0 || H<=0){
+            flag=false;
+            throw new Exception("Breadth and height must be positive");
+        }
+    }
+    
+    catch(Exception e){
+        System.out.println(e);
+        System.exit(0);
+    }
+}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
+
